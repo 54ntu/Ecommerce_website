b42a42f7653f386c1e498c6c1c4c5a0b3da13473
54ntu <chaudharysantaram8@gmail.com>
2025-01-04 00:46:34 +0545

    updated

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -21,7 +21,7 @@ try {
 
 }
 
-sequelize.sync({ force: false, alter: true }).then(() => { console.log("synced") })  //force true garda migrate hunxa...so whenever we made changes into the table that time only we need to make this force true 
+sequelize.sync({ force: false, alter: false }).then(() => { console.log("synced") })  //force true garda migrate hunxa...so whenever we made changes into the table that time only we need to make this force true 
 //when force is set to true for migration it will delete all the existing data of the table
 //alter true garda table maa changes vako matra update hunxa without any loss in data of the table
 
